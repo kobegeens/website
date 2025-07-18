7b615c0f1b695f196a6f5f3e80139fc8b892e562
kobegeens <kobegeens@proton.me>
2025-07-19 00:07:49 +0200

    layout and css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	let { children } = $props();
+    import "/src/app.css"
+</script>
+
+<nav>
+	<a href="#">home</a>
+	<a href="#">about</a>
+</nav>
+
+<main> 
+    {@render children()}
+</main>
+
+<footer>
+    <p>Copyright (c) 2025 Kobe Geens</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,2 @@
-Hey! This is my website made using Sveltekit, come back later.
+<h1>Kobe Geens</h1>
+<h2>Hey! This is my website made using Sveltekit, come back later.</h2>
