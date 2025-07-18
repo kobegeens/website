1f06d12e90c20a6dd9a7f42bc23cb67d575bdf56
kobegeens <kobegeens@proton.me>
2025-07-19 01:01:12 +0200

    index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Kobe Geens</h1>
-<h2>Hey! This is my website made using Sveltekit, come back later.</h2>
+<p>Hey! This is my website made using Sveltekit, come back later.</p>
+
+<hr>
+
+<h2>Projects</h2>
+<p>img+txt</p>
+<p>img+txt</p>
+<p>img+txt</p>
+<p>See more</p>
